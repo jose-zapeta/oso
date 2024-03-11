bca86144b25fdc3f7731a95e2e0ab8f14a3db3e0
PanteraRosa <reylidavid.345@gmail.com>
2024-03-11 12:31:30 -0600

    ruben

---
+++ index (1).js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector(".btn")
+const navMenu = document.querySelector(".nav-menu")
+toggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("nav-menvi");
+});
